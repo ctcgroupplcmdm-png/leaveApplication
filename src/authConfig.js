4967e3f627680138d5ee3d0de39f4715940140b8
NicolasM <108172138+mikellas1983@users.noreply.github.com>
2025-09-25 08:53:16 +0300

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,10 +1,10 @@
 export const msalConfig = {
   auth: {
     clientId: "9635bca0-a248-42ae-8537-4fd8a5b40682", // App registration ID
-    authority: "https://ctcemployees.b2clogin.com/ctcemployees.onmicrosoft.com.onmicrosoft.com/B2C_1_Sign",
+    authority: "https://ctcemployees.b2clogin.com/ctcemployees.onmicrosoft.com/B2C_1_Sign",
     knownAuthorities: ["https://ctcemployees.b2clogin.com"],
-    redirectUri: "http://localhost:3000",
-    postLogoutRedirectUri: "http://localhost:3000"
+    redirectUri: "https://white-field-0d7086703.2.azurestaticapps.net/",
+    postLogoutRedirectUri: "https://white-field-0d7086703.2.azurestaticapps.net/"
   }
 };
 
