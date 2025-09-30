0f0b5e25aa12558384185cf66182f859d2c525ac
NicolasM <108172138+mikellas1983@users.noreply.github.com>
2025-09-30 11:44:00 +0300

    Update UserInfo.js

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,27 +1,47 @@
-import React, { useEffect, useState } from "react";
 import { useMsal } from "@azure/msal-react";
-import useGraphPhone from "./useGraphPhone";
+import { loginRequest } from "./authConfig";
+import { useEffect, useState } from "react";
 
 function UserInfo() {
-  const { accounts } = useMsal();
+  const { instance, accounts } = useMsal();
   const account = accounts[0];
-  const { getPhone } = useGraphPhone();
-
-  const [graphProfile, setGraphProfile] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     if (account) {
-      getPhone().then(setGraphProfile).catch(console.error);
+      // Get token silently
+      instance
+        .acquireTokenSilent({
+          ...loginRequest,
+          account: account
+        })
+        .then((response) => {
+          // Call Microsoft Graph
+          return fetch("https://graph.microsoft.com/v1.0/me", {
+            headers: {
+              Authorization: `Bearer ${response.accessToken}`,
+            },
+          });
+        })
+        .then((res) => res.json())
+        .then((data) => {
+          setUser(data);
+        })
+        .catch((error) => {
+          console.error("Graph error:", error);
+        });
     }
-  }, [account, getPhone]);
+  }, [account, instance]);
 
   if (!account) return <p>Not signed in</p>;
+  if (!user) return <p>Loading user info...</p>;
 
   return (
     <div>
       <p><b>Username:</b> {account.username}</p>
-      <p><b>Name:</b> {graphProfile?.displayName || account.name}</p>
-      <p><b>Phone:</b> {graphProfile?.mobilePhone || "Not available"}</p>
+      <p><b>Name:</b> {user.displayName}</p>
+      <p><b>Email:</b> {user.mail || user.userPrincipalName}</p>
+      <p><b>Phone:</b> {user.mobilePhone || "Not available"}</p>
     </div>
   );
 }
