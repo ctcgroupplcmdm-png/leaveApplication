2454e41274de6b86819607f9c12e2fa75ee0cc44
NicolasM <108172138+mikellas1983@users.noreply.github.com>
2025-10-02 08:27:00 +0300

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "6cc3fada-c6e4-409f-bb05-0c5c1c299879", // App registration ID
+    clientId: "0ad1f969-a1d5-477b-9519-794fc056f122", // App registration ID
     authority: "https://ctcemployees.b2clogin.com/ctcemployees.onmicrosoft.com/B2C_1_Sign",
     knownAuthorities: ["ctcemployees.b2clogin.com"],
     redirectUri: "https://zealous-meadow-05ff82703.2.azurestaticapps.net/",
