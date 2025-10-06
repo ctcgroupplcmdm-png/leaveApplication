4bc80d0a8050138c6b7e9a959801f7996e18970b
NicolasM <108172138+mikellas1983@users.noreply.github.com>
2025-10-06 15:45:30 +0300

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     clientId: "9635bca0-a248-42ae-8537-4fd8a5b40682", // App registration ID
     authority: "https://ctcemployees.b2clogin.com/ctcemployees.onmicrosoft.com/B2C_1_Sign",
     knownAuthorities: ["ctcemployees.b2clogin.com"],
-    redirectUri: "https://zealous-meadow-05ff82703.2.azurestaticapps.net/",
+    redirectUri: "https://portal.ctcgroup.com/",
     postLogoutRedirectUri: "https://portal.ctcgroup.com/"
   }
 };
