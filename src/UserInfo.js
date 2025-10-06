bc3c268f697f33c953bb7d1780b08bf956d9dd54
NicolasM <108172138+mikellas1983@users.noreply.github.com>
2025-10-06 09:41:08 +0300

    Update UserInfo.js

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -61,8 +61,9 @@ function UserInfo() {
         <div>
           <h2 style={styles.title}>Welcome, {displayName}</h2>
           <p style={styles.info}><b>Employee ID:</b> {employeeId}</p>
-		  <p style={styles.info}><b>Object ID:</b> {objectId}</p>
-		  <p style={styles.info}><b>Phone:</b> {phone}</p>
+<p style={styles.info}><b>Object ID:</b> {objectId}</p>
+<p style={styles.info}><b>Phone:</b> {phone}</p>
+
         </div>
         <button style={styles.logoutButton} onClick={logout}>
           Logout
