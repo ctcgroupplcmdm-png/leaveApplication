d24663556642affef4ec1f3a63c6273928156d68
NicolasM <108172138+mikellas1983@users.noreply.github.com>
2025-10-14 09:28:10 +0300

    Update PersonalInfo.js

--- src/PersonalInfo.js
+++ src/PersonalInfo.js
@@ -26,8 +26,7 @@ import apex from "./assets/logos/apex.png";
 import nks from "./assets/logos/nks.png";
 import limni from "./assets/logos/limni.png";
 
-const location = useLocation();
-const forceUpdate = location.state?.forceUpdate || false;
+
 
 const companyLogos = {
   "Argosy Trading Company Ltd": argosy,
@@ -79,6 +78,8 @@ const withCurrentOption = (options, current) => {
 function PersonalInfo() {
   const { instance, accounts } = useMsal();
   const navigate = useNavigate();
+  const location = useLocation();
+const forceUpdate = location.state?.forceUpdate || false;
   const originalData = useRef(null);
   const [userData, setUserData] = useState(null);
   const [formData, setFormData] = useState({
